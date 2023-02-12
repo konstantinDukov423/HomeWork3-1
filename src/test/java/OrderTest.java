cce38629eaa39addad1f38bf7e592f98669c1c68
Kosta <dukov423@mail.ru>
2023-02-12 22:08:48 +0500

    Initial commit

---
+++ src/test/java/OrderTest.java
@@ -0,0 +1,19 @@
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Selenide.*;
+
+public class OrderTest {
+
+    @Test
+    void orderTest() {
+        open("http://localhost:9999");
+        $("input[type='text']").setValue("Константин Дюков");
+        $("input[type='tel']").setValue("+79069935042");
+        $("span[class='checkbox__box']").click();
+        $("button[type='button']").click();
+        $x("//p[contains(text(), 'Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.')]").shouldBe(Condition.visible);
+    }
+}
