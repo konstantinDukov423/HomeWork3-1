03b7a3b21d0a6e5b2407064fdf24a81f95ff1b35
Kosta <dukov423@mail.ru>
2023-02-12 22:25:45 +0500

    Initial commit

--- src/test/java/OrderTest.java
+++ src/test/java/OrderTest.java
@@ -1,7 +1,5 @@
 import com.codeborne.selenide.Condition;
-import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
 
 import static com.codeborne.selenide.Selenide.*;
 
